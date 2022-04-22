38f48686a48b94fb368efd190e5dba80a2cc3cef
ShinMyeongJi <shinmj1207@gmail.com>
2022-04-22 19:54:04 +0000

    init commit

---
+++ apigatewayservice/src/main/java/com/shinmj/apigatewayservice/ApigatewayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shinmj.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayserviceApplication.class, args);
+    }
+
+}
