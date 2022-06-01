79c8082acc4e714a3619db32788abcdae152900e
ShinMyeongJi <shinmj1207@gmail.com>
2022-06-01 23:49:44 +0900

    gradle 정보 수정

---
+++ discoveryservice/src/test/java/com/shinmj/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinmj.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
