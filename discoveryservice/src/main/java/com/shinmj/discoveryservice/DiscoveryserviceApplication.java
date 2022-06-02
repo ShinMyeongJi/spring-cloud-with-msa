00d0d25534e19fac04adc55e17dc8c964230bba7
ShinMyeongJi <shinmj1207@gmail.com>
2022-06-02 23:39:30 +0900

    recommit

--- discoveryservice/src/main/java/com/shinmj/discoveryservice/DiscoveryserviceApplication.java
+++ discoveryservice/src/main/java/com/shinmj/discoveryservice/DiscoveryserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryserviceApplication {
-
+    //엥?
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryserviceApplication.class, args);
     }
