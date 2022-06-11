ccb499d0b88bba163850740f3fffe02834c6e9d8
ShinMyeongJi <shinmj1207@gmail.com>
2022-06-11 21:35:42 +0900

    directory composition modified

--- config-service/src/main/java/com/shinmj/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/shinmj/configservice/ConfigServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServiceApplication.class, args);
+        SpringApplication.run(ConfigServiceApplication.class);
     }
-
 }
